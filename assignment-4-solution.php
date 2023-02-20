9d1993df1c939912e2e06e9f68e3631cae973ef0
islam-monirul <islammonirul.cse@gmail.com>
2023-02-20 16:14:37 +0600

    3rd problem solved

--- assignment-4-solution.php
+++ assignment-4-solution.php
@@ -23,24 +23,39 @@ function concatString(string $str1, string $str2):string{
 
 
 /* =======================================================
+    3. function to remove first and last element from an array
+    and return remaining elements as a new array 
+========================================================== */
+function removeLastAndFirst($arr){
+    array_shift($arr);
+    array_pop($arr);
+
+    return $arr;
+}
+
+
+/* =======================================================
     Result checking for each 5 self defined functions
     ======================================================= */
 
 /* Result checking for function 1 */
 $strs = array("Monir", "Sho", "Po", "Shojiboji", "taki");
-echo "============== Before Sorting ============== \n";
-foreach($strs as $str){
-    echo "$str \n";
-}
-
 // sorting the array of strings
 usort($strs, "sortArrayofStrings");
-
-echo "============== After Sorting ==============\n";
+echo "\n\n============== Answer of problem 1 ==============\n\n";
 foreach($strs as $str){
-    echo "$str \n";
+    echo $str." ";
 }
 
 
 /* Result checking for function 2 */
+echo "\n\n============== Answer of problem 2 ==============\n\n";
 echo concatString("Monirul","Islam");
+
+
+/* Result checking for function 2 */
+$answer3 = removeLastAndFirst(array(1,2,3,4,5));
+echo "\n\n============== Answer of problem 3 ==============\n\n";
+foreach($answer3 as $item){
+    echo $item." ";
+}
